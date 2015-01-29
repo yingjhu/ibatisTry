8257d9dfcef5fe4dda9d609202feec0bc20525a8
yingjhu <yingjhu@hotmail.com>
2015-01-29 16:00:54 +0800

    Add update example

---
+++ src/test/java/IbatisUpdate.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
+public class IbatisUpdate {
+	public static void main(String[] args) throws IOException, SQLException {
+		Reader rd = Resources.getResourceAsReader("SqlMapConfig.xml");
+		SqlMapClient smc = SqlMapClientBuilder.buildSqlMapClient(rd);
+
+		/* This would update one record in Employee table. */
+		System.out.println("Going to update record.....");
+		Employee rec = new Employee();
+		rec.setId(1);
+		rec.setFirstName("Roma");
+		smc.update("Employee.updateFirstName", rec);
+		System.out.println("Record updated Successfully ");
+
+		System.out.println("Going to read records.....");
+		@SuppressWarnings("unchecked")
+		List<Employee> ems = (List<Employee>) smc.queryForList(
+				"Employee.getAll", null);
+		for (Employee e : ems) {
+			System.out.print("  " + e.getId());
+			System.out.print("  " + e.getFirstName());
+			System.out.print("  " + e.getLastName());
+			System.out.print("  " + e.getSalary());
+			System.out.println("");
+		}
+
+		System.out.println("Records Read Successfully ");
+
+	}
+
+}
