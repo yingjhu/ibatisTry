625208044c6216a2db1a73dd0d31dc097c4d7742
yingjhu <yingjhu@hotmail.com>
2015-01-29 15:32:19 +0800

    Add read example with result map

---
+++ src/test/java/IbatisRead.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
+public class IbatisRead {
+	public static void main(String[] args) throws IOException, SQLException {
+		Reader rd = Resources.getResourceAsReader("SqlMapConfig.xml");
+		SqlMapClient smc = SqlMapClientBuilder.buildSqlMapClient(rd);
+
+		/* This would read all records from the Employee table. */
+		System.out.println("Going to read records.....");
+		List<Employee> ems = (List<Employee>) smc.queryForList(
+				"Employee.getAll", null);
+		Employee em = null;
+		for (Employee e : ems) {
+			System.out.print("  " + e.getId());
+			System.out.print("  " + e.getFirstName());
+			System.out.print("  " + e.getLastName());
+			System.out.print("  " + e.getSalary());
+			em = e;
+			System.out.println("");
+		}
+
+		System.out.println("Records Read Successfully ");
+
+	}
+
+}
